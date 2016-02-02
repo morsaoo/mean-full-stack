4d8cb6d87ddc9c9d60cd6d7f062aec4679ff23bd
Alexandre Ribeiro da Silva <alexandre@wolven.com.br>
2016-02-01 23:37:57 -0200

    4.20 A diretiva ng-model e filtragem da lista - aprendido

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,6 +1,7 @@
 // config/express.js
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 module.exports = function() {
   var app = express();
   // configuração de ambiente
@@ -9,11 +10,15 @@ module.exports = function() {
   app.use(express.static('./public'));
   app.set('view engine', 'ejs');
   app.set('views','./app/views');
-
+  // novos middlewares
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
+  
   load('models', {cwd: 'app'})
-    .then('controllers')
-    .then('routes')
-    .into(app);
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
   return app;
 };
